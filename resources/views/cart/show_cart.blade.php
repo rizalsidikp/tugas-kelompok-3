a61007765b1ae69bd5ad533ace622014c6b9ce9b
rizalsidikp <rizalsidikp24@gmail.com>
2023-04-30 20:22:56 +0700

    finish ui chart

--- resources/views/cart/show_cart.blade.php
+++ resources/views/cart/show_cart.blade.php
@@ -1,35 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Carts</title>
-</head>
-
-<body>
-    @foreach ($carts as $cart)
-        <img src="{{ asset('storage/images/' . $cart->product->gambar) }}" alt="" height="100px">
-        <p>Nama: {{ $cart->product->nama }}</p>
-        <p>Amount: {{ $cart->amount }}</p>
-        <form action="{{ route('update_cart', $cart) }}" method="POST">
-            @method('patch')
+@extends('layout/layout')
+@section('layout-style')
+@endsection
+@section('layout-title')
+My Chart -
+@endsection
+@section('layout-content')
+<div class="row page-title">
+    <div class="col-md-8">
+        <h2>My Chart - List Product</h2>
+    </div>
+</div>
+<table class="table table-striped mb-5">
+    <thead>
+        <tr>
+            <th scope="col" class="text-center">No</th>
+            <th scope="col" class="text-center">Gambar</th>
+            <th scope="col" class="text-center">Nama</th>
+            <th scope="col" class="text-center">Amount</th>
+            <th scope="col" class="text-center" colspan="2">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @if(count($carts) === 0)
+        <tr>
+            <td colspan="6" align="center">Data not found</td>
+        </tr>
+        @endif
+        @foreach ($carts as $key => $cart)
+        <tr>
+            <td align="center">{{ $key + 1 }}</th>
+            <td align="center"><img src="{{ asset('storage/images/' . $cart->product->gambar) }}" alt="" height="100px">
+            </td>
+            <td>{{$cart->product->nama}}</td>
+            <td align="center">{{$cart->amount}}</td>
+            <td align="right">
+                <form action="{{ route('update_cart', $cart) }}" method="POST">
+                    @method('patch')
+                    @csrf
+                    <div class="row justify-end">
+                        <div class="col-md-auto no-padding">
+                            <input type="number" name="amount" class="form-control-sm" value={{ $cart->amount }}>
+                        </div>
+                        <div class="col-md-auto">
+                            <button type="submit" class="btn btn-success btn-sm"> Update Amount</button>
+                        </div>
+                    </div>
+                </form>
+            </td>
+            <td>
+                <form action="{{ route('delete_cart', $cart) }}" method="post">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@if(count($carts) > 0)
+<div class="row">
+    <div class="col-md-12 text-right">
+        <form action="{{ route('checkout') }}" method="post">
             @csrf
-            <input type="number" name="amount" value={{ $cart->amount }}>
-            <button type="submit"> Update Amount</button>
+            <button type="submit" class="btn btn-primary">Checkout</button>
         </form>
-        <form action="{{ route('delete_cart', $cart) }}" method="post">
-            @method('delete')
-            @csrf
-            <button type="submit">Delete</button>
-        </form>
-    @endforeach
-
-    <form action="{{ route('checkout') }}" method="post">
-        @csrf
-        <button type="submit">Checkout</button>
-    </form>
-</body>
-
-</html>
+    </div>
+</div>
+@endif
+@endsection
+@section('layout-scripts')
+@endsection
